b71762ce16c97fe170effcd6504642cdc504961a
Aidan <aidan.jayatunge@student.tdsb.on.ca>
2024-05-03 11:27:33 -0400

    Update ReactionTimeGame.java
    testing branch

--- Minigame/src/ReactionTimeGame.java
+++ Minigame/src/ReactionTimeGame.java
@@ -1,4 +1,23 @@
 //see how fast you can press enter. Do it too slow you lose
+
+    import java.util.Random;
+    import java.util.Scanner;
+    import javax.swing.JOptionPane;
+
 public class ReactionTimeGame {
     
-}
+        public static void main(String[] args) throws InterruptedException {
+            
+                Random rndm=new Random();
+
+                JOptionPane.showMessageDialog(null,"3", null ,JOptionPane.INFORMATION_MESSAGE);
+                Thread.sleep(1000);
+    
+                JOptionPane.showMessageDialog(null,"2", null ,JOptionPane.INFORMATION_MESSAGE);
+                Thread.sleep(1000);
+    
+                JOptionPane.showMessageDialog(null,"1", null ,JOptionPane.INFORMATION_MESSAGE);
+                Thread.sleep(rndm.nextInt(10000));
+        }
+    }
+
