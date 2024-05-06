617a93015a5a48cb719e4c365de2791b62be6f65
Aidan <162153155+aidan-sj@users.noreply.github.com>
2024-05-06 17:06:14 -0400

    Updated comments

--- Minigame/src/ReactionTimeGame.java
+++ Minigame/src/ReactionTimeGame.java
@@ -1,4 +1,4 @@
-//see how fast you can press enter. Do it too slow you lose
+//see how fast you can click 'OK'. Do it too slow you lose
 
 import java.time.Duration;
 import java.time.Instant;
@@ -10,41 +10,40 @@ public class Main {
         
             Random rndm=new Random();
             
-            JOptionPane.showMessageDialog(null, "Get ready to click 'OK' after the countdown hits 1");
+            JOptionPane.showMessageDialog(null, "Get ready to click 'OK' after the countdown hits 1. Be careful, if you are too slow you may not get any points!");//briefly explain game
     
-            JOptionPane.showMessageDialog(null,"3...", null ,JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null,"3...", null ,JOptionPane.INFORMATION_MESSAGE);//start countdown with one second intervals
             Thread.sleep(1000);
 
             JOptionPane.showMessageDialog(null,"2...", null ,JOptionPane.INFORMATION_MESSAGE);
             Thread.sleep(1000);
 
-            JOptionPane.showMessageDialog(null,"1...", null ,JOptionPane.INFORMATION_MESSAGE);
-            Thread.sleep(rndm.nextInt(10000)); //remember to change back to higher digit
+            JOptionPane.showMessageDialog(null,"1...", null ,JOptionPane.INFORMATION_MESSAGE);//end countdown
+            Thread.sleep(rndm.nextInt(10000)); //random delay to make game less predictable and more interesting
 
 
-            //-----------------------------------------------------------------------------------------------------------------------------------
             
-                Instant starts = Instant.now();
+                Instant starts = Instant.now();//mark starting point of period of time
                 
                 JOptionPane.showMessageDialog(null,"Click!", null ,JOptionPane.INFORMATION_MESSAGE);
         
-                Instant ends = Instant.now();
+                Instant ends = Instant.now();//mark ending point of period of time
 
-                Duration duration = java.time.Duration.between(starts, ends);
+                Duration duration = java.time.Duration.between(starts, ends);//calculate total duration
 
                 JOptionPane.showMessageDialog(null, "Your reaction time was " + duration.toMillis() + " milliseconds");
 
 
 
-                long score = 10000 - duration.toMillis()*5;
-                    if(score<0)
+                long score = 10000 - duration.toMillis()*5;//calculate score
+                    if(score<0)//determine if player will have a negative score
                     {
-                        JOptionPane.showMessageDialog(null, "You were too slow! Try again next time.");
+                        JOptionPane.showMessageDialog(null, "You were too slow! Try again next time.");//display if player score is negative
                     }
 
                     else
                     {
-                        JOptionPane.showMessageDialog(null, "Your score was " + score + " points!");
+                        JOptionPane.showMessageDialog(null, "Your score was " + score + " points!");//point display for player
                     }
 
                 
